8048555103649a37d57bf6ec5b62b02c94226fa8
Alexandre Reys <alexandre@mytaverse.com>
2023-08-12 18:45:48 -0300

    change projects page to use new page pattern

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -1,7 +1,36 @@
+import Head from 'next/head'
+import { parseCookies } from 'nookies'
+import { GetServerSideProps } from 'next'
+import { getAPIClient } from '../services/axios'
+import DashboardContent from '../components/ProjetosContent'
+import Nav from '../components/nav'
+
 export default function Projects() {
   return (
-    <h1>
-      Projects
-    </h1 >
+    <div>
+      <Head>
+        <title>Projects</title>
+      </Head>
+      <Nav />
+      <DashboardContent />
+    </div>
   )
+}
+
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+  const apiClient = getAPIClient(ctx);
+  const { 'nextauth.token': token } = parseCookies(ctx);
+
+  if (!token) {
+    return {
+      redirect: {
+        destination: '/',
+        permanent: false,
+      }
+    }
+  }
+
+  return {
+    props: {}
+  }
 }
