efd0b6942abd094610d4a84762eb07d616d432c8
Alexandre Reys <alexandre@mytaverse.com>
2023-08-12 19:54:21 -0300

    create new page

---
+++ src/components/TeamContent.tsx
@@ -0,0 +1,18 @@
+export default function TeamContent() {
+  return (
+    <>
+      <header className="bg-white shadow">
+        <div className="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+          <h1 className="text-3xl font-bold text-gray-900">Team</h1>
+        </div>
+      </header>
+      <main>
+        <div className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
+          <h1>
+            Team Content
+          </h1>
+        </div>
+      </main>
+    </>
+  )
+}
