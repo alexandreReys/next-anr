7fbbd3d25eb20f426aab2f8c17187e69e373620d
Alexandre Nunes Reys <anr.alexandre@gmail.com>
2023-09-10 20:36:24 -0300

    change title

--- src/pages/Cep.tsx
+++ src/pages/Cep.tsx
@@ -9,7 +9,7 @@ export default function Dashboard() {
   return (
     <div>
       <Head>
-        <title>Dashboard</title>
+        <title>Cep</title>
       </Head>
       <Nav />
       <Cep />
@@ -21,7 +21,7 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
   const apiClient = getAPIClient(ctx);
   const { 'nextauth.token': token } = parseCookies(ctx);
   if (!token) {
-    return {redirect: {destination: '/',permanent: false,}}
+    return { redirect: { destination: '/', permanent: false, } }
   }
-  return {props: {}}
+  return { props: {} }
 }
